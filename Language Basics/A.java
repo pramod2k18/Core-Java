fe0d1814a8233ae2db179ffe87ed596756de983a
pramod2k18 <pramodparkers@gmail.com>
2020-06-14 06:30:51 +0530

    Command Line Arguments

---
+++ Language Basics/A.java
@@ -0,0 +1,8 @@
+/* Write a Program that accepts two Strings as command line arguments and generate the output in the required format */
+
+import java.util.*;
+public class A{
+	public static void main(String[] args){
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
+}
